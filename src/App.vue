fa75c99aea2c0beac58eaec4c871d6c71eb77e13
cgl <1476972837@qq.com>
2022-11-07 00:02:36 +0800

    完成header部分布局

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,54 @@
 <script setup lang="ts">
+  import {ref} from 'vue'
+  import { Search } from '@element-plus/icons-vue'
+  const searchInput = ref('')
 </script>
 
 <template>
   <div id="main">
     <el-container class="container">
-      <el-header height="50px" class="header">Header</el-header>
+      <el-header height="50px" class="header">
+        <el-row :gutter="10" align="middle" justify="space-around" style="height: 100%;">
+          <el-col :span="6">
+            <div>
+              <el-icon size="20" color="white">
+                <i-ep-headset />
+              </el-icon>
+              <span class="title"> Our Music </span>
+            </div>
+          </el-col>
+          <el-col :span="5" :offset="8">
+            <div>
+              <el-input
+                v-model="searchInput"
+                size="small"
+                placeholder="搜索音乐"
+                :suffix-icon="Search"
+                style="background: #db1212;"
+              />
+            </div>
+          </el-col>
+          <el-col :span="3">
+            <div>
+              <el-dropdown>
+                <div class="user">
+                  <el-avatar
+                  :size="30"
+                    src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+                  />
+                  <span>未登录</span>
+                </div>
+                <template #dropdown>
+                  <el-dropdown-menu>
+                    <el-dropdown-item>登录</el-dropdown-item>
+                    <el-dropdown-item>注销</el-dropdown-item>
+                  </el-dropdown-menu>
+                </template>
+              </el-dropdown>
+            </div>
+          </el-col>
+        </el-row>
+      </el-header>
       <el-container class="down-container">
         <el-aside width="220px" class="aside">Aside</el-aside>
         <el-main>Main</el-main>
@@ -27,6 +71,28 @@
 }
 .header {
   background-color: #c62f2f;
+  /* display: flex;
+  align-items: center;
+  justify-content: space-between; */
+}
+.title {
+  margin-left: 5px;
+  font-size: large;
+  font-weight: 500;
+  color: white;
+}
+.el-input {
+  --el-input-border-radius: 15px;
+}
+.user {
+  display: flex;
+  align-items: center;
+
+  span {
+    margin-left: 5px;
+    color: white;
+    font-size: small;
+  }
 }
 .down-container {
   border-left: 1px solid #d6d6d6;
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
@@ -13,11 +15,22 @@ export default defineConfig({
     vue(),
     vueJsx(),
     AutoImport({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+        ElementPlusResolver(),
+      ],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
-    }),],
+      resolvers: [
+        ElementPlusResolver(),
+        // 自动注册图标组件
+        IconsResolver({
+          enabledCollections: ['ep'],
+        }),],
+    }),
+    Icons({
+      autoInstall: true,
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
