86f5b0d079902ca6252208655e7e9fb3ccc20bd2
cgl <1476972837@qq.com>
2022-11-08 01:05:57 +0800

    实现进度条与音乐播放器功能

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,36 +1,90 @@
-<script setup lang="ts">
-const props = defineProps({
-  progress: {
-    type: Number,
-    default: 12
-  },
-  preText: {
-    type: String,
-    default: '00:00'
-  },
-  postText: {
-    type: String,
-    default: '04:03'
-  },
-  showText: {
-    type: Boolean,
-    default: true
-  },
-  showPointer: {
-    type: Boolean,
-    default: true
+<script setup>
+  import {ref, computed} from 'vue'
+
+  const props = defineProps({
+    progress: {
+      type: Number,
+      default: 0,
+    },
+    preText: {
+      type: String,
+      default: '00:00',
+    },
+    postText: {
+      type: String,
+      default: '05:00',
+    },
+    showText: {
+      type: Boolean,
+      default: true,
+    },
+    showPointer: {
+      type: Boolean,
+      default: true,
+    }
+  })
+
+  const emit = defineEmits(['clickBar', 'startDrag', 'endDrag'])
+
+  const bar = ref(null)
+  
+  function getElementPosition(element) {
+    let top = element.offsetTop //这是获取元素距父元素顶部的距离
+    let left = element.offsetLeft
+    var current = element.offsetParent //这是获取父元素
+    while (current !== null) {
+      //当它上面有元素时就继续执行
+      top += current.offsetTop //这是获取父元素距它的父元素顶部的距离累加起来
+      left += current.offsetLeft
+      current = current.offsetParent //继续找父元素
+    }
+    return {
+      left,
+      top,
+    }
   }
-})
+
+  function getBarPosition(clientX, element) {
+    let clientWidth = element.clientWidth
+    let x = getElementPosition(element)['left']
+    let offsetX = clientX - x
+    let pos = offsetX / clientWidth
+    if (pos < 0) {
+      pos = 0
+    }
+    if (pos > 1) {
+      pos = 1
+    }
+    return pos
+  }
+
+  function clickProgressBar(e) {
+    let pos = getBarPosition(e.clientX, bar.value)
+    emit('startDrag')
+    emit('clickBar', pos)
+
+    document.onmousemove = function(ev) {
+      pos = getBarPosition(ev.clientX, bar.value)
+      emit('clickBar', pos)
+    }
+
+    document.onmouseup = function(){
+      document.onmousemove = ''
+      document.onmouseup = ''
+      emit('endDrag')
+      emit('clickBar', pos)
+    }
+  }
 </script>
 
 <template>
   <div class="progessbar-container">
-    <span v-if="showText">{{preText}}</span>
-    <div class="progressbar">
-        <div class="line" :style="`width: ${progress >= 100 ? 100:progress}%;`"></div>
-        <div v-if="showPointer" class="pointer" :style="`left: ${progress >= 100 ? 100:progress}%;`"></div>
+    <span v-if="props.showText">{{props.preText}}</span>
+    <div ref="bar" class="progressbar" @mousedown="clickProgressBar">
+        <div class="line" :style="`width: ${props.progress >= 100 ? 100 : props.progress}%;`"></div>
+        <div v-if="props.showPointer" class="pointer" :style="`left: ${props.progress >= 100 ? 100 : props.progress}%;`"></div>
     </div>
-    <span v-if="showText">{{postText}}</span>
+    <span v-if="props.showText">{{props.postText}}</span>
   </div>
 </template>
 
@@ -46,6 +100,7 @@ const props = defineProps({
     background-color: #c2c2c4;
     border-radius: 13px;
     margin: 0 15px;
+    cursor: pointer;
 }
 .progressbar .line {
     background-color:#e83c3c;
