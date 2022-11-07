5d13f0cb1e6f7117ff22664e601018c6baa52e21
cgl <1476972837@qq.com>
2022-11-07 12:15:23 +0800

    将header放入单独的页面文件

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,12 @@
 <script setup lang="ts">
-  import {ref} from 'vue'
-  import { Search } from '@element-plus/icons-vue'
-  const searchInput = ref('')
+  import Header from './views/Header.vue'
 </script>
 
 <template>
   <div id="main">
     <el-container class="container">
-      <el-header height="50px" class="header">
-        <el-row :gutter="10" align="middle" justify="space-around" style="height: 100%;">
-          <el-col :span="6">
-            <div>
-              <el-icon size="20" color="white">
-                <i-ep-headset />
-              </el-icon>
-              <span class="title"> Our Music </span>
-            </div>
-          </el-col>
-          <el-col :span="5" :offset="8">
-            <div>
-              <el-input
-                v-model="searchInput"
-                size="small"
-                placeholder="搜索音乐"
-                :suffix-icon="Search"
-                style="background: #db1212;"
-              />
-            </div>
-          </el-col>
-          <el-col :span="3">
-            <div>
-              <el-dropdown>
-                <div class="user">
-                  <el-avatar
-                  :size="30"
-                    src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
-                  />
-                  <span>
-                    未登录
-                    <el-icon style="margin-left: 0;">
-                      <i-ep-arrow-down />
-                    </el-icon>
-                  </span>
-                </div>
-                <template #dropdown>
-                  <el-dropdown-menu>
-                    <el-dropdown-item>登录</el-dropdown-item>
-                    <el-dropdown-item>注销</el-dropdown-item>
-                  </el-dropdown-menu>
-                </template>
-              </el-dropdown>
-            </div>
-          </el-col>
-        </el-row>
+      <el-header height="50px" style="background-color: #c62f2f;">
+        <Header />
       </el-header>
       <el-container class="down-container">
         <el-aside width="220px" class="aside">Aside</el-aside>
@@ -74,31 +28,7 @@
 .container {
   height: 100%;
 }
-.header {
-  background-color: #c62f2f;
-  /* display: flex;
-  align-items: center;
-  justify-content: space-between; */
-}
-.title {
-  margin-left: 5px;
-  font-size: large;
-  font-weight: 500;
-  color: white;
-}
-.el-input {
-  --el-input-border-radius: 15px;
-}
-.user {
-  display: flex;
-  align-items: center;
 
-  span {
-    margin-left: 8px;
-    color: white;
-    font-size: small;
-  }
-}
 .down-container {
   border-left: 1px solid #d6d6d6;
   border-right: 1px solid #d6d6d6;
