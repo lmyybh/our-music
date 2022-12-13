fba44fef8c12e755091c6bb8bcaf19d2274e403a
lmyybh <lmyybh_lazy@163.com>
2022-12-13 21:52:06 +0800

    改变服务器配置方式

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import Icons from 'unplugin-icons/vite'
@@ -8,49 +8,52 @@ import IconsResolver from 'unplugin-icons/resolver'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import { getEnv } from './config'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    AutoImport({
-      resolvers: [
-        ElementPlusResolver(),
-      ],
-    }),
-    Components({
-      resolvers: [
-        ElementPlusResolver(),
-        // 自动注册图标组件
-        IconsResolver({
-          enabledCollections: ['ep'],
-        }),],
-    }),
-    Icons({
-      autoInstall: true,
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 8888,
-    proxy: {
-      '/api': {
-        target: getEnv('dev').api_url,	//实际请求地址
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      },
-      '/server': {
-        target: getEnv('dev').server_url,	//实际请求地址
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/server/, '')
-      },
-    }
-  },
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  console.log(mode)
+  return {
+    plugins: [
+      vue(),
+      vueJsx(),
+      AutoImport({
+        resolvers: [
+          ElementPlusResolver(),
+        ],
+      }),
+      Components({
+        resolvers: [
+          ElementPlusResolver(),
+          // 自动注册图标组件
+          IconsResolver({
+            enabledCollections: ['ep'],
+          }),],
+      }),
+      Icons({
+        autoInstall: true,
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      host: '0.0.0.0',
+      port: 8888,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,	//实际请求地址
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        },
+        '/server': {
+          target: env.VITE_SERVER_URL,	//实际请求地址
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/server/, '')
+        },
+      }
+    },
+  }
 })
